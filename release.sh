ccad3157f7ffc42bbe0574b1e10cd5b4a56533ae
Arkayenro <arkayenro@gmail.com>
2025-03-09 12:53:49 +1100

    updates

--- release.sh
+++ release.sh
@@ -85,6 +85,58 @@ if [[ ${BASH_VERSINFO[0]} -lt 4 ]] || [[ ${BASH_VERSINFO[0]} -eq 4 && ${BASH_VER
 	exit 1
 fi
 
+
+install_pandoc() {
+	# Install pandoc for WoWI changelog output ([md -> html] -> bbcode)
+
+	command -v pandoc &>/dev/null && return 0
+
+	if [[ -n "$WOWI_API_TOKEN" && "$GITHUB_REF" == "refs/tags/"* && "$INPUT_PANDOC" == "true" ]]; then
+		sudo apt-get install -yq pandoc &>/dev/null && echo -e "##[group]Install pandoc\\n[command]pandoc --version\\n$( pandoc --version )\\n##[endgroup]"
+	fi
+}
+
+install_subversion() {
+	# Install subversion if the pkgmeta includes external svn repos
+
+	command -v svn &>/dev/null && return 0
+
+	local pkgmeta_file
+	local OPTIND=1
+	# shellcheck disable=SC2086
+	while getopts ":celLzusSop:dw:a:r:t:g:m:n:" opt; do
+		case $opt in
+			m)
+				pkgmeta_file="${GITHUB_WORKSPACE}/${OPTARG}"
+				[[ ! -f "$pkgmeta_file" ]] && return 0
+				;;
+			*)
+		esac
+	done
+	if [[ -z "$pkgmeta_file" ]]; then
+		if [[ -f "${GITHUB_WORKSPACE}/.pkgmeta" ]]; then
+			pkgmeta_file="${GITHUB_WORKSPACE}/.pkgmeta"
+		elif [[ -f "${GITHUB_WORKSPACE}/pkgmeta.yaml" ]]; then
+			pkgmeta_file="${GITHUB_WORKSPACE}/pkgmeta.yaml"
+		else
+			return 0
+		fi
+	fi
+
+	# check type then url, exit 1 if no matches
+	if yq -e '.externals | ( (.[] | select(.type == "svn")) or (with_entries(.value |= .url) | .[] | select(test(".*/trunk(?:/|$)"))) )' < "$pkgmeta_file" &>/dev/null; then
+		# echo "::notice title=GitHub Actions Change::The runner image for ubuntu-latest is being updated to use ubuntu-24.04, which no longer includes subversion." \
+		#      "Update your workflow \"run-as\" to use ubuntu-22.04 directly or add a step to install subversion to continue support for svn repositories."
+		sudo apt-get install -yq subversion &>/dev/null && echo -e "##[group]Install subversion\\n[command]svn --version\\n$( svn --version )\\n##[endgroup]"
+	fi
+}
+
+if [[ -n $GITHUB_ACTIONS ]]; then
+	install_pandoc
+	install_subversion
+fi
+
+
 # Game versions for uploading
 declare -A game_flavors=( ["retail"]="mainline" ["classic"]="classic" ["bcc"]="bcc" ["wrath"]="wrath" ["cata"]="cata" )
 declare -A game_versions=()
